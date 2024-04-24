9034f925c88b09781a20c75c511c25d4b8b3aad5
hungdhv97 <hungdhv97@gmail.com>
2024-04-25 02:00:29 +0900

    SHT-006 Implement carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,30 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
     return (
-        //<main className={styles.main}>
         <div>
             <ul>
                 <li>
                     <Link href="/home">
                         Giới thiệu
                     </Link>
-
                 </li>
                 <li>
                     <Link href="/image">
                         Hoạt động
                     </Link>
-
                 </li>
                 <li>
                     <Link href="/quiz">
                         Trắc nghiệm
                     </Link>
                 </li>
+                <li>
+                    <Link href="/image">
+                        Hình ảnh
+                    </Link>
+                </li>
             </ul>
         </div>
-    )
+    );
 }
