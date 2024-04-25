80bc8100d844766bf3e356222b6af08fb218dd4b
hungdhv97 <hungdhv97@gmail.com>
2024-04-26 01:13:14 +0900

    SHT-007 Fix Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,31 @@ import Link from 'next/link';
 
 export default function Home() {
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link href="/introduction">
-                        Giới thiệu
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/activity">
-                        Hoạt động
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/quiz">
-                        Trắc nghiệm
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/image">
-                        Hình ảnh
-                    </Link>
-                </li>
-            </ul>
+        <div className="container flex flex-col items-center justify-evenly h-96">
+            <Link
+                className="rounded-2xl bg-red-500 text-white p-5 w-52 text-center"
+                href="/introduction"
+            >
+                Giới thiệu
+            </Link>
+            <Link
+                className="rounded-2xl bg-green-500 text-white p-5 w-52 text-center"
+                href="/activity"
+            >
+                Hoạt động
+            </Link>
+            <Link
+                className="rounded-2xl bg-blue-500 text-white p-5 w-52 text-center"
+                href="/quiz"
+            >
+                Trắc nghiệm
+            </Link>
+            <Link
+                className="rounded-2xl bg-purple-500 text-white p-5 w-52 text-center"
+                href="/image"
+            >
+                Hình ảnh
+            </Link>
         </div>
     );
 }
