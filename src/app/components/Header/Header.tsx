be0af01f58d800748761950267d774239e3155ec
hungdhv97 <hungdhv97@gmail.com>
2024-04-26 00:23:50 +0900

    SHT-007 Export to ImageCarousel

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
     return (
-        <header>
+        <header className="container">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a
--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { Carousel } from 'flowbite-react';
 import React from 'react';
+import { ImageCarousel } from '@/app/components/ImageCarousel/ImageCarousel';
 
 const Image = () => {
     const router = useRouter();
@@ -9,45 +9,8 @@ const Image = () => {
         router.push('/');
     };
     return (
-        <div className="h-screen">
-            <Carousel>
-                <img
-                    src="/images/cot-moc-403.jpg"
-                    alt="..."
-                />
-                <img
-                    src="/images/cot-moc.jpg"
-                    alt="..."
-                />
-                <img
-                    src="/images/lu-que-ky-son.jpg"
-                    alt="..."
-                />
-                <img
-                    src="/images/lu-que-ky-son-2.jpg"
-                    alt="..."
-                />
-                <img
-                    src="/images/tuyen-truyen.jpg"
-                    alt="..."
-                />
-                <img
-                    src="/images/dien-bien-hoa-binh.jpg"
-                    alt="..."
-                />
-                <img
-                    src="/images/lu-que-ky-son.jpg"
-                    alt="..."
-                />
-                <img
-                    src="/images/lu-que-ky-son-2.jpg"
-                    alt="..."
-                />
-                <img
-                    src="/images/Tongket.jpg"
-                    alt="..."
-                />
-            </Carousel>
+        <div className="h-96">
+            <ImageCarousel />
         </div>
     )
         ;
