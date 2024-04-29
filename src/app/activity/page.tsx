7c076d045626b7110d8e73098488ff57878f8f9b
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2024-04-29 16:19:43 +0900

    SHT-014 Apply videos

--- src/app/activity/page.tsx
+++ src/app/activity/page.tsx
@@ -1,7 +1,38 @@
+'use client';
+
+import { VideoCard, VideoCardProps } from '@/app/components/VideoCard/VideoCard';
+
+
+const videos: VideoCardProps[] = [
+    {
+        title: 'Tiêu đề 1',
+        content: 'Nội dung 1',
+        url: 'https://drive.google.com/file/d/1JNkndDFvyj6RAICaBmcNxTAUaLxL9tPf/preview',
+    }, {
+        title: 'Tiêu đề 2',
+        content: 'Nội dung 2',
+        url: 'https://drive.google.com/file/d/1bOGi2lkhT_Gk15EeJDyxlPNTHdlHimQX/preview',
+    }, {
+        title: 'Tiêu đề 3',
+        content: 'Nội dung 3',
+        url: 'https://drive.google.com/file/d/10fdMsEGCIONaYwDMpGNRzGej9LmXwFi7/preview',
+    },
+];
+
+
 const Activity = () => {
     return (
         <div className="container">
-            Hoạt động
+            <div className="grid grid-cols-3 gap-4">
+                {videos.map((video, idx) => (
+                    <VideoCard
+                        key={idx}
+                        title={video.title}
+                        url={video.url}
+                        content={video.content}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
