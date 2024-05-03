21d60ed5bbb154067a8909f42efe96849f54f0ac
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2024-05-03 17:05:13 +0700

    SHT-039 Refactor code

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -1,65 +1,52 @@
 import Link from 'next/link';
 
+const references = [
+    {
+        url: 'https://hoilhpn.org.vn/documents/246915/6360445/33cd.signed.pdf/21845a53-05da-4e07-a6b9-dfc30b4a4531',
+        text: 'Công điện của Thủ tướng Chính phủ số 33/CĐ-TTg về tăng cường bảo đảm an toàn thông tin mạng',
+    },
+    {
+        url: 'https://hoilhpn.org.vn/documents/20182/0/_VB_1713259272222_VB_3222-CV%202024.pdf/5d060488-8411-4cd8-92fb-316a2c7b617e',
+        text: 'v/v bảo đảm trật tự, an toàn giao thông, nhân dịp nghỉ lễ Giỗ tổ Hùng Vương, lễ 30/4-/1/5 và cao điểm du lịch hè 2024',
+    },
+    {
+        url: 'https://hoilhpn.org.vn/tin-chi-tiet/-/chi-tiet/%C4%91ieu-le-hoi-lien-hiep-phu-nu-viet-nam-46760-6801.html',
+        text: 'Điều lệ Hội Liên hiệp phụ nữ Việt Nam khóa XVIII',
+    },
+    {
+        url: 'https://hoilhpn.org.vn/documents/20182/0/_VB_1692093800943_VB_2494-Q%C4%90-HTPNPTKT.pdf/b9612f2c-37e3-467f-b812-393e111a291c',
+        text: 'Quyết định phê duyệt danh sách các Dự án khởi nghiệp được lựa chọn vào vòng chung kết cấp vùng Cuộc thi \'Phụ nữ khởi nghiệp, phát huy tài nguyên bản địa\' năm 2023',
+    },
+];
+
 export default function Guide() {
     return (
         <div className="container">
-            <div
-                className="text-purple-500 text-4xl font-bold m-4"
-            >
+            <div className="text-purple-500 text-4xl font-bold m-4">
                 Hướng dẫn thi trắc nghiệm
             </div>
-            <div className="grid md:grid-cols-5 grid-cols-1 gap-8 mb-8">
-                <img
-                    src="https://res.cloudinary.com/ezcode97/image/upload/v1714494660/sinh_hoat_hoi/huong_dan/1.jpg"
-                    title="nhấn 3 gạch/họ tên/đơn vị/vào thi"
-                />
-                <img
-                    src="https://res.cloudinary.com/ezcode97/image/upload/v1714494660/sinh_hoat_hoi/huong_dan/2.jpg"
-                    title="chọn cuộc thi"
-                />
-                <img
-                    src="https://res.cloudinary.com/ezcode97/image/upload/v1714494660/sinh_hoat_hoi/huong_dan/3.jpg"
-                    title="làm bài thi"
-                />
-                <img
-                    src="https://res.cloudinary.com/ezcode97/image/upload/v1714494660/sinh_hoat_hoi/huong_dan/4.jpg"
-                    title="chụp kết quả gửi giám khảo"
-                />
+            <div className="grid md:grid-cols-4 grid-cols-1 gap-8 mb-8">
+                {Array.from({ length: 4 }, (_, index) => (
+                    <img
+                        key={index}
+                        src={`https://res.cloudinary.com/ezcode97/image/upload/v1714494660/sinh_hoat_hoi/huong_dan/${index + 1}.jpg`}
+                        title={`Bước ${index + 1}`}
+                    />
+                ))}
             </div>
             <div className="border-blue-900">
-                <h1 className="text-2xl font-bold my-4">TÀI LIỆU THAM KHẢO</h1>
-                <p className="my-4">
-                    <Link
-                        href="https://hoilhpn.org.vn/documents/246915/6360445/33cd.signed.pdf/21845a53-05da-4e07-a6b9-dfc30b4a4531"
-                        className="hover:underline hover:text-blue-700 text-blue-500"
-                    >
-                        {'->'} Công điện của Thủ tướng Chính phủ số 33/CĐ-TTg về tăng cường bảo đảm an toàn
-                        thông tin mạng
-                    </Link>
-                    <br />
-                    <Link
-                        href="https://hoilhpn.org.vn/documents/20182/0/_VB_1713259272222_VB_3222-CV%202024.pdf/5d060488-8411-4cd8-92fb-316a2c7b617e"
-                        className="hover:underline hover:text-blue-700 text-blue-500"
-                    >
-                        {'->'} v/v bảo đảm trật tự, an toàn giao thông, nhân dịp nghỉ lễ Giỗ tổ Hùng Vương, lễ
-                        30/4-/1/5 và cao điểm du lịch hè 2024
-                    </Link>
-                    <br />
-                    <Link
-                        href="https://hoilhpn.org.vn/tin-chi-tiet/-/chi-tiet/%C4%91ieu-le-hoi-lien-hiep-phu-nu-viet-nam-46760-6801.html"
-                        className="hover:underline hover:text-blue-700 text-blue-500"
-                    >
-                        {'->'} Điều lệ Hội Liên hiệp phụ nữ Việt Nam khóa XVIII
-                    </Link>
-                    <br />
-                    <Link
-                        href="https://hoilhpn.org.vn/documents/20182/0/_VB_1692093800943_VB_2494-Q%C4%90-HTPNPTKT.pdf/b9612f2c-37e3-467f-b812-393e111a291c"
-                        className="hover:underline hover:text-blue-700 text-blue-500"
-                    >
-                        {'->'} Quyết định phê duyệt danh sách các Dự án khởi nghiệp được lựa chọn vào vòng chung
-                        kết cấp vùng Cuộc thi &apos;Phụ nữ khởi nghiệp, phát huy tài nguyên bản địa&apos; năm 2023
-                    </Link>
-                </p>
+                <div className="text-2xl font-bold my-4">TÀI LIỆU THAM KHẢO</div>
+                <div className="my-4">
+                    {references.map((reference, index) => (
+                        <Link
+                            key={index}
+                            href={reference.url}
+                            className="hover:underline hover:text-blue-700 text-blue-500 block"
+                        >
+                            {'->'} {reference.text}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -1,82 +1,68 @@
 'use client';
-
 import Link from 'next/link';
 
-export default function Introduction() {
+const introduction_title: string = 'Giới thiệu';
+
+interface SectionProps {
+    title: string;
+    content: string[];
+}
+
+const sections: SectionProps[] = [
+    {
+        title: 'HỆ THỐNG TỔ CHỨC VÀ TƯ CÁCH PHÁP NHÂN CỦA CÁC CẤP HỘI',
+        content: [
+            '1. Hội Liên hiệp Phụ nữ Việt Nam gồm 4 cấp:',
+            'a. Cấp Trung ương;',
+            'b. Cấp tỉnh (bao gồm Hội Liên hiệp Phụ nữ các tỉnh, thành phố trực thuộc Trung ương và tương đương);',
+            'c. Cấp huyện (bao gồm Hội Liên hiệp Phụ nữ các quận, huyện, thị xã, thành phố thuộc tỉnh và tương đương);',
+            'd. Cấp cơ sở (bao gồm Hội Liên hiệp Phụ nữ các xã, phường, thị trấn và tương đương).',
+            '2. Trung ương Hội; Hội Liên hiệp Phụ nữ các tỉnh, thành phố trực thuộc Trung ương; Hội Liên hiệp Phụ nữ các quận, huyện, thị xã, thành phố thuộc tỉnh; Hội Liên hiệp Phụ nữ các xã, phường, thị trấn có tư cách pháp nhân độc lập, hoạt động trong khuôn khổ Hiến pháp và pháp luật.',
+        ],
+    },
+    {
+        title: 'CƠ QUAN LÃNH ĐẠO VÀ CƠ QUAN CHUYÊN TRÁCH CÁC CẤP HỘI',
+        content: [
+            '1. Cơ quan lãnh đạo các cấp Hội:',
+            'a. Cơ quan lãnh đạo cao nhất của Hội là Đại hội đại biểu Phụ nữ toàn quốc;',
+            'b. Cơ quan lãnh đạo ở mỗi cấp Hội là Đại hội đại biểu hoặc Đại hội toàn thể hội viên của cấp đó;',
+            'c. Giữa hai kỳ đại hội, cơ quan lãnh đạo mỗi cấp Hội là Ban Chấp hành cùng cấp;',
+            'd. Giữa hai kỳ họp Ban Chấp hành, cơ quan lãnh đạo là Đoàn Chủ tịch hoặc Ban Thường vụ cùng cấp.',
+            '2. Cơ quan chuyên trách các cấp Hội:',
+            'Cơ quan chuyên trách Trung ương Hội; cơ quan chuyên trách Hội Liên hiệp Phụ nữ các tỉnh, thành phố trực thuộc Trung ương và tương đương; cơ quan chuyên trách Hội Liên hiệp Phụ nữ các quận, huyện, thị xã, thành phố thuộc tỉnh và tương đương là cơ quan tham mưu, giúp việc cho Ban Chấp hành, Đoàn Chủ tịch hoặc Ban Thường vụ cùng cấp.',
+        ],
+    },
+];
+
+// Component
+function Section({ title, content }: SectionProps) {
+    return (
+        <div className="p-4 bg-amber-100 text-blue-800 font-normal text-justify m-4">
+            <p className="text-bold text-white text-center bg-green-800 p-4 mb-4">{title}</p>
+            <div className="indent-4 sm:indent-8">
+                {content.map((item, index) => (
+                    <div key={index}>{item}</div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+function Introduction() {
     return (
         <div className="container">
-            <div
-                className="text-red-500 text-4xl font-bold mx-4 mt-4"
-            >
-                GIỚI THIỆU
-            </div>
+            <div className="text-red-500 text-4xl font-bold mx-4 mt-4">{introduction_title}</div>
             <div className="grid md:grid-cols-2 grid-cols-1">
-                <div className="p-4 bg-amber-100 text-blue-800 font-normal text-justify m-4">
-                    <p className="text-bold text-white text-center bg-green-800 p-4 mb-4">
-                        * HỆ THỐNG TỔ CHỨC VÀ TƯ CÁCH PHÁP NHÂN CỦA CÁC CẤP HỘI
-                    </p>
-                    <div className="indent-4 sm:indent-8">
-                        <div>
-                            1. Hội Liên hiệp Phụ nữ Việt Nam gồm 4 cấp:
-                        </div>
-                        <ul>
-                            <li>a. Cấp Trung ương;</li>
-                            <li>b. Cấp tỉnh (bao gồm Hội Liên hiệp Phụ nữ các tỉnh, thành phố trực
-                                thuộc Trung ương và tương đương);
-                            </li>
-                            <li>c. Cấp huyện (bao gồm Hội Liên hiệp Phụ nữ các quận, huyện, thị xã,
-                                thành phố thuộc tỉnh và tương đương);
-                            </li>
-                            <li>d. Cấp cơ sở (bao gồm Hội Liên hiệp Phụ nữ các xã, phường, thị trấn và tương đương).
-                            </li>
-                        </ul>
-                        <div>
-                            2. Trung ương Hội; Hội Liên hiệp Phụ nữ các tỉnh, thành phố trực thuộc Trung ương; Hội Liên
-                            hiệp Phụ nữ các quận, huyện, thị xã, thành phố thuộc tỉnh; Hội Liên hiệp Phụ nữ các xã,
-                            phường, thị trấn có tư cách pháp nhân độc lập, hoạt động trong khuôn khổ Hiến pháp và pháp
-                            luật.
-                        </div>
-                    </div>
-                </div>
-                <div className="p-4 bg-amber-100 text-blue-800 font-normal text-justify m-4">
-                    <p className="text-bold text-white text-center bg-green-800 p-4 mb-4">
-                        * CƠ QUAN LÃNH ĐẠO VÀ CƠ QUAN CHUYÊN TRÁCH CÁC CẤP HỘI
-                    </p>
-                    <div className="indent-4 sm:indent-8">
-                        <div>1. Cơ quan lãnh đạo các cấp Hội:</div>
-                        <ul>
-                            <li>
-                                a. Cơ quan lãnh đạo cao nhất của Hội là Đại hội đại biểu Phụ nữ
-                                toàn quốc;
-                            </li>
-                            <li>
-                                b. Cơ quan lãnh đạo ở mỗi cấp Hội là Đại hội đại biểu hoặc Đại hội toàn
-                                thể hội viên của cấp đó;
-                            </li>
-                            <li>
-                                c. Giữa hai kỳ đại hội, cơ quan lãnh đạo mỗi cấp Hội là Ban Chấp hành
-                                cùng cấp;
-                            </li>
-                            <li>
-                                d. Giữa hai kỳ họp Ban Chấp hành, cơ quan lãnh đạo là Đoàn Chủ tịch hoặc
-                                Ban Thường vụ cùng cấp.
-                            </li>
-                        </ul>
-                        <div>2. Cơ quan chuyên trách các cấp Hội:</div>
-                        <ul>
-                            <li>Cơ quan chuyên trách Trung ương Hội; cơ quan chuyên trách Hội Liên hiệp
-                                Phụ nữ các tỉnh, thành phố trực thuộc Trung ương và tương đương; cơ quan chuyên trách
-                                Hội Liên hiệp Phụ nữ các quận, huyện, thị xã, thành phố thuộc tỉnh và tương đương là cơ
-                                quan tham mưu, giúp việc cho Ban Chấp hành, Đoàn Chủ tịch hoặc Ban Thường vụ cùng cấp.
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                {sections.map((section, index) => (
+                    <Section
+                        key={index}
+                        title={section.title}
+                        content={section.content}
+                    />
+                ))}
             </div>
             <div className="mb-4">
-                <Link
-                    href="/quiz"
-                >
+                <Link href="/quiz">
                     <img
                         className="rounded-2xl bg-red-500 text-white"
                         src="/images/background.jpg"
@@ -86,3 +72,5 @@ export default function Introduction() {
         </div>
     );
 }
+
+export default Introduction;
